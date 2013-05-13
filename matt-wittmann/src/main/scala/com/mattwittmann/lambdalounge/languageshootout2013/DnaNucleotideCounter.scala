86cdecc5e498f504df078639a87ab3aa3e908e4d
Matt Wittmann <matt@mattwittmann.com>
2013-05-13 14:30:07 -0500

    Moving from scala.collection.mutable.Set to scala.collection.immutable.Set through use of fold

--- matt-wittmann/src/main/scala/com/mattwittmann/lambdalounge/languageshootout2013/DnaNucleotideCounter.scala
+++ matt-wittmann/src/main/scala/com/mattwittmann/lambdalounge/languageshootout2013/DnaNucleotideCounter.scala
@@ -1,11 +1,8 @@
 package com.mattwittmann.lambdalounge.languageshootout2013
 
-import collection.mutable.Map
-
 object DnaNucleotideCounter {
   def count(input: String) = {
-    val map = Map[Char, Int]()
-    input.foreach {c => map += ((c, map.getOrElse(c, 0) + 1))}
+    val map =input.foldLeft(Map[Char, Int]()) {(map, i) => map + ((i, (map.getOrElse(i, 0) + 1)))}
     List('A', 'C', 'G', 'T').seq.map {map.getOrElse(_, 0)}.mkString(" ")
   }
 }
--- matt-wittmann/src/test/scala/com/mattwittmann/lambdalounge/languageshootout2013/UT_DnaNucleotideCounter.scala
+++ matt-wittmann/src/test/scala/com/mattwittmann/lambdalounge/languageshootout2013/UT_DnaNucleotideCounter.scala
@@ -2,6 +2,9 @@ package com.mattwittmann.lambdalounge.languageshootout2013
 
 import org.scalatest.FunSuite
 
+/**
+ * @see [[http://rosalind.info/problems/dna/]]
+ */
 class UT_DnaNucleotideCounter extends FunSuite {
   test("Sample per http://rosalind.info/problems/dna/") {
     assert("20 12 17 21" == DnaNucleotideCounter.count("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"))
