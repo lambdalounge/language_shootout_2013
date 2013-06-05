9d9ed87351813c74ceaca2258eed4008c3481154
puredanger <alexdmiller@yahoo.com>
2013-06-04 23:31:19 -0500

    add some fibs

--- puredanger/rabbit/src/rabbit/rabbit.clj
+++ puredanger/rabbit/src/rabbit/rabbit.clj
@@ -1,6 +1,42 @@
 (ns rabbit.rabbit)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; recursive
+(defn fibk [n k]
+  (if (< n 2) 
+    n
+    (+ (fibk (- n 1) k)
+       (* k (fibk (- n 2) k)))))
+
+;; using reduce 
+
+(defn- fibk-reduce' [n k init]
+  (if (zero? n)
+    init
+    (second
+     (reduce (fn [[f2 f1] _] [f1 (+ f1 (* k f2))])
+             [init (inc init)]
+             (range (dec n))))))
+
+(defn fibk-reduce [n k]
+  (fibk-reduce' n k 0))
+
+(defn fibk-reduce-big [n k]
+  (fibk-reduce' n k 0M))
+
+;; using iterate to remove bogus range and _
+
+(defn fibk-iterate' [n k init]
+  (let [all (iterate (fn [[f2 f1]] [f1 (+ f1 (* k f2))])
+                     [init (inc init)])]
+    (nth (map first all) n)))
+
+(defn fibk-iterate [n k]
+  (fibk-iterate' n k 0))
+
+(defn fibk-iterate-big [n k]
+  (fibk-iterate' n k 0M))
+
+;; helper
+
+(defn fseq [f n k]
+  (map #(f % k) (range n)))
