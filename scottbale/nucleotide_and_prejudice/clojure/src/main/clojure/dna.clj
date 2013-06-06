7eeba3835b7e139e3bc421646aad50a5a89700f5
Scott Bale <scott@balehaus.org>
2013-06-06 02:47:57 +0000

    clojure dna

--- scottbale/nucleotide_and_prejudice/clojure/src/main/clojure/dna.clj
+++ scottbale/nucleotide_and_prejudice/clojure/src/main/clojure/dna.clj
@@ -1,22 +1,20 @@
 (ns dna)
 
 (defn count-nucleotides [word]
-  (let [counts {\A 0
-                \C 0
-                \G 0
-                \T 0}]
-    (reduce (fn [counts symb]
-              (let [[k count] (find counts symb)]
-                (apply assoc counts [k (inc count)])))
-            counts
-            word)))
+  (reduce (fn [counts symb]
+            (let [[k count] (find counts symb)]
+              (apply assoc counts [k (inc count)])))
+          {\A 0 \C 0 \G 0 \T 0}
+          word))
 
 (defn formatted-count [counts]
-  (apply str (interpose " " (map (comp str counts) [\A \C \G \T]))))
+  (apply str (interpose " " (map counts [\A \C \G \T]))))
+
+(defn formatted-count [counts]
+  (apply format "%d %d %d %d" (map counts [\A \C \G \T])))
 
 (comment
   
   (formatted-count (count-nucleotides "ACGCATGAAT"))
-
 
   )
